52315a7efd38bb0aa34489098d4611ad4482d793
Mathias Bevers <mathiasbevers@outlook.com>
2023-03-29 11:37:48 +0200

    main basic movements

--- Assets/Code/BallMovement.cs
+++ Assets/Code/BallMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(SphereCollider))]
 public class BallMovement : MonoBehaviour
 {
+	[SerializeField] private bool blockPlayerInput;
 	[SerializeField] private bool isPlayerControlled;
 	[SerializeField] private float maxSpeed = 100.0f;
 	[SerializeField] private float speed = 5.0f;
@@ -19,37 +20,46 @@ public class BallMovement : MonoBehaviour
 		cachedTransform = transform;
 	}
 
-	private void Update() { input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); }
+	private void Update()
+	{
+		if (!isPlayerControlled) { return; }
+
+		if (blockPlayerInput) { return; }
+
+		input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+	}
 
 	private void FixedUpdate() { Move(input); }
 
 	private void Move(Vector3 input)
 	{
-		if (input.magnitude > 0)
+		if (input.magnitude <= 0)
 		{
-			input.Normalize();
-			input *= speed;
-			rigidbody.AddForce(input);
-
-			if (rigidbody.velocity.magnitude <= maxSpeed) { return; }
-
-			rigidbody.velocity *= maxSpeed / rigidbody.velocity.magnitude;
+			rigidbody.velocity *= 0.9f;
 			return;
 		}
 
-		rigidbody.velocity *= 0.9f;
+		input.Normalize();
+		input *= speed;
+		rigidbody.AddForce(input);
+
+		if (rigidbody.velocity.magnitude <= maxSpeed) { return; }
+
+		Vector3 velocity = rigidbody.velocity;
+		float division = maxSpeed / velocity.magnitude;
+		Vector3 newVelocity = velocity * division;
+		rigidbody.velocity = newVelocity;
 	}
 
 	private void OnCollisionEnter(Collision collision)
 	{
 		// This is an other player.
-		if (collision.collider.GetComponent<BallMovement>() == null)
-		{
-			return;
-		}
+		if (collision.collider.GetComponent<BallMovement>() == null) { return; }
 
+		rigidbody.AddExplosionForce(25f, collision.contacts[0].point, 0.5f, 1, ForceMode.Impulse);
+		blockPlayerInput = true;
 
-		Debug.Log("Other player was hit!");
+		CooldownManager.Cooldown(1.5f, () => blockPlayerInput = false);
 	}
 
 	private void OnGUI() { GUI.Label(new Rect(0, 0, 250, 25), $"Velocity: {rigidbody.velocity.magnitude}"); }
