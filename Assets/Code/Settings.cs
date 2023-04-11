b0bc27532257315bbd42cbcaad42d064ba91966d
Amber Kortier <AmberKortier@gmail.com>
2023-04-11 16:15:45 +0200

    New build test!

--- Assets/Code/Settings.cs
+++ Assets/Code/Settings.cs
@@ -7,9 +7,9 @@ using UnityEditor;
 public static class Settings
 {
     public static string preferName = string.Empty;
-    public static IPAddress serverIP = IPAddress.Parse("127.0.0.1");
+    public static IPAddress serverIP = IPAddress.Any; 
     //public static IPAddress serverIP = IPAddress.Any;
-        //IPAddress.Broadcast;
+    //IPAddress.Broadcast;
     public static IPAddress ip = IPAddress.Parse("127.0.0.1");
     public static int port = Convert.ToInt32(25565);
     public const int maxPlayerCount = 4;
