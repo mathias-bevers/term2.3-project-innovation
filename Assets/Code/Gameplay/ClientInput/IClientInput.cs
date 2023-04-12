d633c34b080bf1ce68bdd5c2eb3fe033edb59795
Mathias Bevers <mathiasbevers@outlook.com>
2023-04-12 10:36:28 +0200

    Input modules start

---
+++ Assets/Code/Gameplay/ClientInput/IClientInput.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public interface IClientInput
+{
+	/// <summary>
+	///	Set up the input type. When initialization succeeds, true will be returned.
+	/// </summary>
+	/// <returns>Whether the initialization has succeeded</returns>
+	public bool Initialize();
+
+	public Vector2 GetInput();
+}
